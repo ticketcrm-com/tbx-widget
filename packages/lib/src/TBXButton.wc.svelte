eee117003627aa708f654121d11e9167354a2077
v.rieznikova <v.rieznikova@nodeart.io>
2023-09-21 03:15:06 +0300

    fix(ga): took params from cookies

--- packages/lib/src/TBXButton.wc.svelte
+++ packages/lib/src/TBXButton.wc.svelte
@@ -29,7 +29,7 @@
 />
 
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { onMount, afterUpdate } from "svelte";
 
   export let widgetUrl: string | undefined;
   export let eventHash: string | undefined;
@@ -49,95 +49,94 @@
   let ga: { id: string; session_id: string; client_id: string }[];
 
   const getLink = () => {
-    const url = new URL(widgetUrl || "https://widget.ticketcrm.com/");
-    const urlParams = new URLSearchParams(window.location.search);
-    const utm_source = window.location.host.replace("www.", "");
-    const add_params = {
-      ...(eventHash && { widgetHash: eventHash }),
-      ...(abonementHash && { seasonHash: abonementHash }),
-      ...(lang && { lang }),
-      ...(currency && { currency }),
-      ...(thanksUrl && { thank: encodeURIComponent(thanksUrl) }),
-      ...(thanksInvoiceUrl && {
-        thankInvoice: encodeURIComponent(thanksInvoiceUrl),
-      }),
-      ...(sellerUrl && { sellerUrl: encodeURIComponent(sellerUrl) }),
-      ...(homeUrl && { url: encodeURIComponent(homeUrl) }),
-      ...(homeLogoUrl && { logo: encodeURIComponent(homeLogoUrl) }),
-      ...(ga && { ga: JSON.stringify(ga) }),
+    try {
+      const url = new URL(widgetUrl || "https://widget.ticketcrm.com/");
+      const urlParams = new URLSearchParams(window.location.search);
+      const utm_source = window.location.host.replace("www.", "");
+      const add_params = {
+        ...(eventHash && { widgetHash: eventHash }),
+        ...(abonementHash && { seasonHash: abonementHash }),
+        ...(lang && { lang }),
+        ...(currency && { currency }),
+        ...(thanksUrl && { thank: encodeURIComponent(thanksUrl) }),
+        ...(thanksInvoiceUrl && {
+          thankInvoice: encodeURIComponent(thanksInvoiceUrl),
+        }),
+        ...(sellerUrl && { sellerUrl: encodeURIComponent(sellerUrl) }),
+        ...(homeUrl && { url: encodeURIComponent(homeUrl) }),
+        ...(homeLogoUrl && { logo: encodeURIComponent(homeLogoUrl) }),
+        ...(ga?.length && { ga: JSON.stringify(ga) }),
 
-      ...(!utm_source.includes("ticketsbox") && { utm_source }),
-      ...(urlParams.get("utm_source") && {
-        utm_source: urlParams.get("utm_source"),
-      }),
-      ...(urlParams.get("utm_medium") && {
-        utm_medium: urlParams.get("utm_medium"),
-      }),
-      ...(urlParams.get("utm_campaign") && {
-        utm_source: urlParams.get("utm_campaign"),
-      }),
-      ...(urlParams.get("utm_content") && {
-        utm_source: urlParams.get("utm_content"),
-      }),
-    };
-    const new_params = new URLSearchParams([
-      ...Array.from(url.searchParams.entries()),
-      ...Object.entries(add_params),
-    ]).toString();
-    const new_url = new URL(`${url.origin}${url.pathname}?${new_params}`);
-    return new_url.href;
+        ...(!utm_source.includes("ticketsbox") && { utm_source }),
+        ...(urlParams.get("utm_source") && {
+          utm_source: urlParams.get("utm_source"),
+        }),
+        ...(urlParams.get("utm_medium") && {
+          utm_medium: urlParams.get("utm_medium"),
+        }),
+        ...(urlParams.get("utm_campaign") && {
+          utm_source: urlParams.get("utm_campaign"),
+        }),
+        ...(urlParams.get("utm_content") && {
+          utm_source: urlParams.get("utm_content"),
+        }),
+      };
+      const new_params = new URLSearchParams([
+        ...Array.from(url.searchParams.entries()),
+        ...Object.entries(add_params),
+      ]).toString();
+      const new_url = new URL(`${url.origin}${url.pathname}?${new_params}`);
+      return new_url.href;
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  $: ga, (iframeUrl = getLink()); // , console.log("ga, ", ga);
+  $: ga,
+    (iframeUrl = getLink()),
+    ga?.length && console.log("ga synchronized"),
+    ga?.length && console.log("ga, ", ga);
 
   const getGa = () => {
-    window.addEventListener("message", function (e) {
-      if (e.data?.type === "ga") {
-        const ids: string[] = e.data.data[1];
-        if (ids && ids[0]) {
-          const metrics = [
-            new Promise((resolve) => {
-              gtag("get", ids[0], "client_id", resolve);
-            }),
-          ].concat(
-            ids.map(
-              (id) =>
-                new Promise((resolve) => {
-                  gtag("get", id, "session_id", resolve);
-                })
-            )
-          );
+    try {
+      const ids = document.cookie
+        .split("; ")
+        .filter((row) => row.startsWith("_ga_"))
+        .map((el) => el.split("_ga_")[1].split("=")[0]);
 
-          Promise.all(metrics).then(
-            ([client_id, session_id, ...args]: string[]) => {
-              ga = [{ id: ids[0], client_id, session_id }].concat(
-                args.map((session_id, i) => ({
-                  id: ids[i + 1],
-                  client_id,
-                  session_id,
-                }))
-              );
-              console.log("ga synchronized");
-            }
-          );
-        }
-      }
-    });
+      const client_id = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("_ga="))
+        ?.split("=")[1]
+        .split(".")
+        .splice(-2, 2)
+        .join(".");
+
+      const sessions_id = ids.map(
+        (id) =>
+          document.cookie
+            .split("; ")
+            .find((row) => row.startsWith(`_ga_${id}`))
+            ?.split("=")[1]
+            .split(".")
+            .filter((el) => el.length > 5)[0]
+      );
+      ga = ids.map((id, iter) => ({
+        id: `G-${id}`,
+        client_id,
+        session_id: sessions_id[iter],
+      }));
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   onMount(() => {
-    try {
-      if (typeof gtag !== "undefined") {
-        getGa();
-      } else {
-        iframeUrl = getLink();
-      }
-    } catch (e) {
-      console.log(e);
-    }
+    getGa();
+    iframeUrl = getLink();
   });
 
-  // afterUpdate(() => (iframeUrl = getLink()));
+  afterUpdate(() => (iframeUrl = getLink()));
 </script>
 
 <div
--- packages/lib/src/TBXWidget.wc.svelte
+++ packages/lib/src/TBXWidget.wc.svelte
@@ -50,97 +50,100 @@
   let ref: HTMLIFrameElement;
 
   const getLink = () => {
-    const url = new URL(widgetUrl || "https://widget.ticketcrm.com/");
-    const urlParams = new URLSearchParams(window.location.search);
-    const utm_source = window.location.host.replace("www.", "");
-    const add_params = {
-      ...(eventHash && { widgetHash: eventHash }),
-      ...(abonementHash && { seasonHash: abonementHash }),
-      ...(lang && { lang }),
-      ...(currency && { currency }),
-      ...(thanksUrl && { thank: encodeURIComponent(thanksUrl) }),
-      ...(thanksInvoiceUrl && {
-        thankInvoice: encodeURIComponent(thanksInvoiceUrl),
-      }),
-      ...(sellerUrl && { sellerUrl: encodeURIComponent(sellerUrl) }),
-      ...(homeUrl && { url: encodeURIComponent(homeUrl) }),
-      ...(homeLogoUrl && { logo: encodeURIComponent(homeLogoUrl) }),
+    try {
+      const url = new URL(widgetUrl || "https://widget.ticketcrm.com/");
+      const urlParams = new URLSearchParams(window.location.search);
+      const utm_source = window.location.host.replace("www.", "");
+      const add_params = {
+        ...(eventHash && { widgetHash: eventHash }),
+        ...(abonementHash && { seasonHash: abonementHash }),
+        ...(lang && { lang }),
+        ...(currency && { currency }),
+        ...(thanksUrl && { thank: encodeURIComponent(thanksUrl) }),
+        ...(thanksInvoiceUrl && {
+          thankInvoice: encodeURIComponent(thanksInvoiceUrl),
+        }),
+        ...(sellerUrl && { sellerUrl: encodeURIComponent(sellerUrl) }),
+        ...(homeUrl && { url: encodeURIComponent(homeUrl) }),
+        ...(homeLogoUrl && { logo: encodeURIComponent(homeLogoUrl) }),
 
-      ...(!utm_source.includes("ticketsbox") && { utm_source }),
-      ...(urlParams.get("utm_source") && {
-        utm_source: urlParams.get("utm_source"),
-      }),
-      ...(urlParams.get("utm_medium") && {
-        utm_medium: urlParams.get("utm_medium"),
-      }),
-      ...(urlParams.get("utm_campaign") && {
-        utm_source: urlParams.get("utm_campaign"),
-      }),
-      ...(urlParams.get("utm_content") && {
-        utm_source: urlParams.get("utm_content"),
-      }),
-    };
-    const new_params = new URLSearchParams([
-      ...Array.from(url.searchParams.entries()),
-      ...Object.entries(add_params),
-    ]).toString();
-    const new_url = new URL(`${url.origin}${url.pathname}?${new_params}`);
-    return new_url.href;
+        ...(!utm_source.includes("ticketsbox") && { utm_source }),
+        ...(urlParams.get("utm_source") && {
+          utm_source: urlParams.get("utm_source"),
+        }),
+        ...(urlParams.get("utm_medium") && {
+          utm_medium: urlParams.get("utm_medium"),
+        }),
+        ...(urlParams.get("utm_campaign") && {
+          utm_source: urlParams.get("utm_campaign"),
+        }),
+        ...(urlParams.get("utm_content") && {
+          utm_source: urlParams.get("utm_content"),
+        }),
+      };
+      const new_params = new URLSearchParams([
+        ...Array.from(url.searchParams.entries()),
+        ...Object.entries(add_params),
+      ]).toString();
+      const new_url = new URL(`${url.origin}${url.pathname}?${new_params}`);
+      return new_url.href;
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const getGa = () => {
     window.addEventListener("message", function (e) {
-      if (e.data?.type === "loaded") {
-        loaded = true;
-      }
-      if (e.data?.type === "ga") {
-        const ids = e.data.data[1];
-        if (ids && ids[0]) {
-          const metrics = [
-            new Promise((resolve) => {
-              gtag("get", ids[0], "client_id", resolve);
-            }),
-          ].concat(
-            ids.map(
-              (id) =>
-                new Promise((resolve) => {
-                  gtag("get", id, "session_id", resolve);
-                })
-            )
-          );
-
-          Promise.all(metrics).then(([client_id, session_id, ...args]) => {
-            ref.contentWindow.postMessage(
-              {
-                type: "_ga",
-                data: [{ id: ids[0], client_id, session_id }].concat(
-                  args.map((session_id, i) => ({
-                    id: ids[i + 1],
-                    client_id,
-                    session_id,
-                  }))
-                ),
-              },
-              "*"
-            );
-            console.log("ga synchronized");
-          });
+      try {
+        if (e.data?.type === "loaded") {
+          loaded = true;
         }
+        if (e.data?.type === "ga") {
+          const ids = e.data.data[1];
+          if (ids && ids[0]) {
+            const client_id = document.cookie
+              .split("; ")
+              .find((row) => row.startsWith("_ga="))
+              ?.split("=")[1]
+              .split(".")
+              .splice(-2, 2)
+              .join(".");
+            const sessions_id = ids
+              .map(
+                (id) =>
+                  document.cookie
+                    .split("; ")
+                    .find((row) => row.startsWith(`_ga_${id.split("-")[1]}`))
+                    ?.split("=")[1]
+                    .split(".")
+                    .filter((el) => el.length > 5)[0]
+              )
+              .filter((el) => el);
+            if (client_id && sessions_id.length) {
+              ref.contentWindow.postMessage(
+                {
+                  type: "_ga",
+                  data: ids.map((id, iter) => ({
+                    id,
+                    client_id,
+                    session_id: sessions_id[iter],
+                  })),
+                },
+                "*"
+              );
+              console.log("ga synchronized");
+            }
+          }
+        }
+      } catch (error) {
+        console.log(error);
       }
     });
   };
 
   onMount(() => {
-    try {
-      if (typeof gtag !== "undefined") {
-        getGa();
-      } else {
-        loaded = true;
-      }
-      iframeUrl = getLink();
-    } catch (e) {
-      console.log(e);
-    }
+    getGa();
+    iframeUrl = getLink();
   });
 
   afterUpdate(() => (iframeUrl = getLink()));
